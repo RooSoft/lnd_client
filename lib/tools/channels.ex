e4099ab13db4eced83dbba16b3dc3d023117016f
CODL.CO <info@codl.co>
2021-07-04 07:42:15 -0500

    Added get_large_channels
    And other little changes

--- lib/tools/channels.ex
+++ lib/tools/channels.ex
@@ -1,6 +1,6 @@
 defmodule LndClient.Tools.Channels do
 
-  def get_small_channels(maximum_size \\ 4_499_000, is_active \\ true) do
+  def get_small_channels(maximum_size \\ 3_999_000, is_active \\ true) do
     %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels(is_active)
     channels
     |> Stream.filter(fn channel -> channel.capacity < maximum_size end)
@@ -10,6 +10,18 @@ defmodule LndClient.Tools.Channels do
   defp print_small_channels(channel) do
     %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
     IO.puts "#{node_alias} has only a #{channel.capacity} sat capacity"
+  end
+
+  def get_large_channels(minimum_size \\ 10_000_000, is_active \\ true) do
+    %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels(is_active)
+    channels
+    |> Stream.filter(fn channel -> channel.capacity > minimum_size end)
+    |> Enum.each(&print_large_channels/1)
+  end
+
+  defp print_large_channels(channel) do
+    %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
+    IO.puts "#{node_alias} has a #{channel.capacity} sat capacity"
   end
 
   def keep_smaller_channels(channels, size_in_sats) do
@@ -25,20 +37,20 @@ defmodule LndClient.Tools.Channels do
 
   defp print_names_and_capacity(channel) do
     %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
-    IO.puts("#{node_alias} Capacity:#{channel.capacity}, LocalBalance:#{channel.local_balance}")
+    IO.puts "#{node_alias} Capacity:#{channel.capacity}, LocalBalance:#{channel.local_balance}"
   end
 
   def get_stagnant_channels() do
     %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels()
     channels
-    |> Stream.filter(fn channel -> (channel.total_satoshis_sent == 0 || channel.total_satoshis_received == 0) end)
+    |> Stream.filter(fn channel -> (channel.total_satoshis_sent < 2000 || channel.total_satoshis_received < 2000) end)
     |> Stream.filter(fn channel -> channel.local_balance > (channel.capacity * 0.45) end)
     |> Enum.each(&print_stagnant_channel/1)
   end
 
   defp print_stagnant_channel(channel) do
     %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
-    IO.puts("#{node_alias} with #{channel.local_balance} sats is stagnant")
+    IO.puts "#{node_alias} with #{channel.local_balance} sats is stagnant"
   end
 
   def get_inactive_channels() do
@@ -51,6 +63,20 @@ defmodule LndClient.Tools.Channels do
 
   defp print_inactive_channels(channel) do
     %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
-    IO.puts("#{node_alias} with #{channel.local_balance} sats is inactive")
+    IO.puts "#{node_alias} with #{channel.local_balance} sats is inactive"
+  end
+
+  def get_poor_channels() do
+    %Lnrpc.ListChannelsResponse{ channels: channels } = LndClient.get_channels()
+    channels
+    |> Stream.filter(fn channel -> (channel.total_satoshis_sent < 2000 || channel.total_satoshis_received < 2000) end)
+    |> Stream.filter(fn channel -> channel.capacity < 3_900_000 end)
+    |> Stream.filter(fn channel -> channel.local_balance > (channel.capacity * 0.45) end)
+    |> Enum.each(&print_poor_channels/1)
+  end
+
+  defp print_poor_channels(channel) do
+    %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
+    IO.puts "#{node_alias} is only #{channel.capacity} sats and is stagnant"
   end
 end
