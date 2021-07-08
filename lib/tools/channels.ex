aa2c3bbd577d3d369d6ced19328ca098a64159aa
CODL.CO <info@codl.co>
2021-07-08 13:35:27 -0500

    Added update_channel_policy

--- lib/tools/channels.ex
+++ lib/tools/channels.ex
@@ -1,8 +1,7 @@
 defmodule LndClient.Tools.Channels do
 
   def get_small_channels(maximum_size \\ 3_999_000, is_active \\ true) do
-    %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels(is_active)
-    channels
+    LndClient.get_channels.channels
     |> Stream.filter(fn channel -> channel.capacity < maximum_size end)
     |> Enum.each(&print_small_channels/1)
   end
@@ -13,8 +12,7 @@ defmodule LndClient.Tools.Channels do
   end
 
   def get_large_channels(minimum_size \\ 10_000_000, is_active \\ true) do
-    %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels(is_active)
-    channels
+    LndClient.get_channels.channels
     |> Stream.filter(fn channel -> channel.capacity > minimum_size end)
     |> Enum.each(&print_large_channels/1)
   end
@@ -30,8 +28,7 @@ defmodule LndClient.Tools.Channels do
   end
 
   def display_names_and_capacity() do
-    %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels()
-    channels
+    LndClient.get_channels.channels
     |> Enum.each(&print_names_and_capacity/1)
   end
 
@@ -41,8 +38,7 @@ defmodule LndClient.Tools.Channels do
   end
 
   def get_stagnant_channels() do
-    %Lnrpc.ListChannelsResponse{channels: channels} = LndClient.get_channels()
-    channels
+    LndClient.get_channels.channels
     |> Stream.filter(fn channel -> (channel.total_satoshis_sent < 2000 || channel.total_satoshis_received < 2000) end)
     |> Stream.filter(fn channel -> channel.local_balance > (channel.capacity * 0.45) end)
     |> Enum.each(&print_stagnant_channel/1)
@@ -54,8 +50,7 @@ defmodule LndClient.Tools.Channels do
   end
 
   def get_inactive_channels() do
-    %Lnrpc.ListChannelsResponse{ channels: channels } = LndClient.get_channels()
-    channels
+    LndClient.get_channels.channels
     |> Stream.filter(fn channel -> channel.active == :false end)
     |> Stream.filter(fn channel -> channel.local_balance != 0 end)
     |> Enum.each(&print_inactive_channels/1)
@@ -67,8 +62,7 @@ defmodule LndClient.Tools.Channels do
   end
 
   def get_poor_channels() do
-    %Lnrpc.ListChannelsResponse{ channels: channels } = LndClient.get_channels()
-    channels
+    LndClient.get_channels.channels
     |> Stream.filter(fn channel -> (channel.total_satoshis_sent < 2000 || channel.total_satoshis_received < 2000) end)
     |> Stream.filter(fn channel -> channel.capacity < 3_900_000 end)
     |> Stream.filter(fn channel -> channel.local_balance > (channel.capacity * 0.45) end)
@@ -78,5 +72,16 @@ defmodule LndClient.Tools.Channels do
   defp print_poor_channels(channel) do
     %Lnrpc.NodeInfo{ node: %Lnrpc.LightningNode{ alias: node_alias } } = LndClient.get_node_info(channel.remote_pubkey)
     IO.puts "#{node_alias} is only #{channel.capacity} sats and is stagnant"
+  end
+
+  def update_channel_policy(max_htlc_msat, base_fee_msat, fee_rate, output_index, csv_delay, txid) do
+    LndClient.update_channel_policy(%{
+      max_htlc_msat: max_htlc_msat,
+      base_fee_msat: base_fee_msat,
+      fee_rate: fee_rate,
+      output_index: output_index,
+      time_lock_delta: csv_delay,
+      txid: txid
+      })
   end
 end
