16c8c44ea8e939ce32d9bd09a5e4a606cf32a4b3
marc@roosoft.com <marc@roosoft.com>
2022-11-10 13:19:07 -0500

    configured for hex publication

--- mix.exs
+++ mix.exs
@@ -1,13 +1,18 @@
 defmodule LndClient.MixProject do
   use Mix.Project
 
+  @version "0.1.7"
+
   def project do
     [
       app: :lnd_client,
-      version: "0.1.6",
+      version: @version,
+      description: "Connect to an LND instance, issue commands and subscribe to live information",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -18,9 +23,27 @@ defmodule LndClient.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      # The main page in the docs
+      main: "LndClient",
+      source_ref: @version,
+      source_url: "https://github.com/roosoft/lnd_client"
+    ]
+  end
+
+  def package do
+    [
+      maintainers: ["Marc Lacoursière"],
+      licenses: ["UNLICENCE"],
+      links: %{"GitHub" => "https://github.com/roosoft/lnd_client"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
       {:grpc, "~> 0.5.0"},
       {:protobuf, "~> 0.11.0"}
     ]
